c4aba9411c968d66535caa05f9310ce200c152f1
BeautyNadolice <strsasa@gmail.com>
2026-07-17 12:18:52 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzt05Q17aZVe0Up6C5AVSfzG5M2XmVgr5qDaJ7OerhFE084bSVQwYezgTAV36Xth0bkAw/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxB8193hyTP02d4bF3E500vEtHsOImgD_nmuZnZk1xiZu8In8sm1f8JUWBtXQ144dETlg/exec";
 const ALLOWED_EMAIL = "vasha_jena@gmail.com"; 
 let currentUserEmail = null;
 
