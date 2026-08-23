d276d73757feebe787fa6a421b6f98217a8b76de
BeautyNadolice <strsasa@gmail.com>
2026-08-23 06:12:23 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "daria-index-v21-1";
+const CORE = [
+  "./",
+  "./index.html",
+  "./style.css?v=21.1",
+  "./booking.js?v=21.1",
+  "./manifest.webmanifest?v=14.0",
+  "./icons/app-192.png",
+  "./icons/app-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => undefined));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const request = event.request;
+  if (request.method !== "GET") return;
+
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+  if (url.pathname.includes("/admin/")) return;
+
+  event.respondWith(
+    fetch(request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(request, copy)).catch(() => undefined);
+        return response;
+      })
+      .catch(() => caches.match(request).then(cached => cached || caches.match("./index.html")))
+  );
+});
